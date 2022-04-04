7287e62e58efbac99593b813bac191ae0a18f19c
Ujjwal <ujjwal2022.cs19@nsut.ac.in>
2022-04-04 21:19:30 +0530

    go server for websocket connection

--- go.mod
+++ go.mod
@@ -4,9 +4,11 @@ go 1.16
 
 require (
 	github.com/gorilla/websocket v1.5.0
+	github.com/labstack/echo/v4 v4.7.2 // indirect
 	golang.org/x/net v0.0.0-20220325170049-de3da57026de // indirect
 	golang.org/x/sys v0.0.0-20220328115105-d36c6a25d886 // indirect
 	google.golang.org/genproto v0.0.0-20220328180837-c47567c462d1 // indirect
 	google.golang.org/grpc v1.45.0
 	google.golang.org/protobuf v1.28.0
+	nhooyr.io/websocket v1.8.7 // indirect
 )
---
+++ server/server.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{}
+
+func sendMessage(conn *websocket.Conn){
+		var err error
+    msg := `Hi, the handshake is complete!`
+
+		if err = conn.WriteMessage(websocket.TextMessage, []byte(msg)); err != nil {
+				log.Println(err)
+				return
+		}else{
+				fmt.Println("Message sent")
+		}
+
+
+}
+
+func reader(conn *websocket.Conn) {
+    for {
+        messageType, p, err := conn.ReadMessage()
+        if err != nil {
+            log.Println(err)
+            return
+        }
+        fmt.Println(string(p))
+				sendMessage(conn)
+
+        if err := conn.WriteMessage(messageType, p); err != nil {
+            log.Println(err)
+            return
+        }
+
+    }
+}
+
+func serveWs(w http.ResponseWriter, r *http.Request) {
+
+    fmt.Println(r.Host)
+		CheckOrigin := func(r *http.Request) bool {
+			return true
+		}
+		upgrader.CheckOrigin = CheckOrigin
+
+    ws, err := upgrader.Upgrade(w, r, nil)
+    if err != nil {
+        log.Println(err)
+  }
+    reader(ws)
+}
+
+func setupRoutes() {
+  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Simple Server")
+  })
+
+    http.HandleFunc("/ws", serveWs)
+}
+
+func main() {
+
+    setupRoutes()
+    http.ListenAndServe(":8080", nil)
+
+}
